41e01e46df309d11a3bb0e8b1c1db6fdc5f78a61
Gnana Prakash M <gnanaprakash2918@gmail.com>
2023-07-15 20:41:33 +0530

    Playround() method is added

--- script.js
+++ script.js
@@ -3,4 +3,40 @@ function getComputerChoice() {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection) {
+  //return 1 for user ,0 for computer,-1 for tie
+  playerSelection = playerSelection.toLowerCase();
+  computerSelection = computerSelection.toLowerCase();
+
+  if (playerSelection == "rock") {
+    if (computerSelection == "paper") {
+      return 1;
+    } else if (computerSelection == "scissors") {
+      return 0;
+    } else {
+      return -1;
+    }
+  } else if (playerSelection == "paper") {
+    if (computerSelection == "rock") {
+      return 1;
+    } else if (computerSelection == "scissors") {
+      return 0;
+    } else {
+      return -1;
+    }
+  } else if (playerSelection == "scissors") {
+    if (computerSelection == "rock") {
+      return 0;
+    } else if (computerSelection == "paper") {
+      return 1;
+    } else {
+      return -1;
+    }
+  }
+}
+
+console.log(playRound("paper", getComputerChoice()));
+console.log(playRound("paper", getComputerChoice()));
+console.log(playRound("paper", getComputerChoice()));
+console.log(playRound("paper", getComputerChoice()));
+console.log(playRound("paper", getComputerChoice()));
